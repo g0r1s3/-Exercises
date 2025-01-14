a3e1a6cc9b7dc4154dd172ffe254e9005afbec3f
Andreas Schmidt <andreas.schmidt.lth@proton.me>
2025-01-14 13:57:03 +0100

    Basic functions

---
+++ Web/ImageChanger/app.js
@@ -0,0 +1,58 @@
+const upload = document.getElementById("upload");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const grayscaleBtn = document.getElementById("grayscale");
+const flipHorizontalBtn = document.getElementById("flipHorizontal");
+const downloadBtn = document.getElementById("download");
+
+let img = new Image();
+
+upload.addEventListener("change", (event) => {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      img.src = reader.result;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+img.onload = () => {
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.drawImage(img, 0, 0);
+};
+
+grayscaleBtn.addEventListener("click", () => {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    data[i] = avg; // Red
+    data[i + 1] = avg; // Green
+    data[i + 2] = avg; // Blue
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+});
+
+flipHorizontalBtn.addEventListener("click", () => {
+  const tempCanvas = document.createElement("canvas");
+  const tempCtx = tempCanvas.getContext("2d");
+  tempCanvas.width = canvas.width;
+  tempCanvas.height = canvas.height;
+  tempCtx.scale(-1, 1);
+  tempCtx.drawImage(canvas, -canvas.width, 0);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(tempCanvas, 0, 0);
+});
+
+downloadBtn.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.download = "edited-image.png";
+  link.href = canvas.toDataURL();
+  link.click();
+});
